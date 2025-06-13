349a353004744fec2f82afaa77c8687c9e641493
Ramashish6867 <ashishgond565656@gmail.com>
2025-06-13 13:58:02 +0530

    Add files via upload

---
+++ AIML_2C_FirstRestAPI/AIML_2C_FirstRestAPI/src/main/java/com/kiet/AIML_2C_FirstRestAPI/controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.kiet.AIML_2C_FirstRestAPI.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody; 
+import com.kiet.AIML_2C_FirstRestAPI.model.Student;
+import com.kiet.AIML_2C_FirstRestAPI.service.StudentService;
+
+//@RestController
+public class StudentController {
+	private StudentService service;
+
+	public StudentController(StudentService service) {
+		super();
+		this.service = service;
+	}
+
+
+	@GetMapping("/students/{id}")
+	public Student getStudent( @PathVariable int id) {
+		return service.retrieveStudentById(id);
+	}
+	@DeleteMapping("/students/{id}")
+	public void removeStudent( @PathVariable int id) {
+		service.remove(id);
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getAllStudent() {
+		return service.getAll();
+	}
+	
+	@PostMapping("/students")
+	public Student addStudent(@RequestBody Student student) {
+		return service.saveStudent(student);
+	}
+}
